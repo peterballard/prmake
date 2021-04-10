1f32ea02a09506d3a666c52e27a11ca1eb98b61d
Peter Ballard <peter.ballard@adelaide.edu.au>
2021-04-10 22:10:10 +0930

    renamed prmake.py to pmake, add setup.py, improve README

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name="prmake",
+      version="0.4.2",
+      description="make preprocessor",
+      author="Peter Ballard",
+      author_email="pb@peterballard.org",
+      url="https://github.com/peterballard/prmake",
+      py_modules=[],
+      scripts=["prmake"],
+      )
